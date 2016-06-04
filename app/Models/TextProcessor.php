4846045e3e7a382e626b6ace5dc9aaee42af51e3
Vladislav Kolodka <vladkolodka@gmail.com>
2016-06-05 00:59:00 +0300

    TextProcessor v2

--- app/Models/TextProcessor.php
+++ app/Models/TextProcessor.php
@@ -1,80 +1,77 @@
 <?php
+
 namespace Qualifier\Models;
 
-use Qualifier\Http\Models;
+use Qualifier\Exceptions\JsonException;
 use Symfony\Component\HttpFoundation\File\UploadedFile as UploadedFile;
+use Qualifier\Http\Models\Language;
+use Qualifier\Http\Models\Topic;
+use Qualifier\Http\Models\Document;
+use Qualifier\Http\Models\Word;
 use vladkolodka\linearAlgebra\SingularValueDecomposition;
 
 class TextProcessor{
-    private $topics = null;
+    private $text;
+    private $text_name;
 
-    /**
-     * TextProcessor constructor.
-     * @param UploadedFile $file File from request
-     * @param $text_language string language_name
-     */
     public function __construct(UploadedFile $file, $text_language) {
+        $this->language = Language::OfLang($text_language);
 
-        // get language
-        $language = Models\Language::OfLang($text_language);
+        $this->text = (new TextParser($file->getPathname(), $file->getClientOriginalExtension()))->getText();
+        $file_name = $file->getClientOriginalName();
 
-        // parse text
-        $parser = new TextParser($file->getPathname(), $file->getClientOriginalExtension());
-
-//        dd($parser->getText());
-
-        $existing_document = Models\Document::where('hash', 'fff')->first();
-        // if document exists in database, return its topic
-        if($existing_document){
-            $this->topics = [$existing_document->topic()->get()];
-            return;
-        }
-//        Models\Document::where('hash', md5($parser->getText()))->first();
-
-        // get text words
-        $normalizer = new TextNormalizer($parser->getText(), $language);
-
-        // build matrix for SVD
-        $matrixBuilder = new DocumentWordMatrixCreator($normalizer->getWords(), $language);
-
-        // singular value decomposition
-        $svd = new SingularValueDecomposition($matrixBuilder->getMatrix());
-
-        $matrix = $svd->getV();
-
-        // categorize text
-        $qualifier = new TextCategorization($matrix);
-
-        // get topics by document ids
-        $this->topics = Models\Topic::find($matrixBuilder->getTopicsByDocumentIndex($qualifier->getResults()));
+        $this->text_name = substr($file_name, 0, strpos($file_name, '.'));
     }
 
     /**
      * Creates new document in database
      * @param $words array
-     * @param $topic Models\Topic
+     * @param $topic Topic
+     * @param string $text_name
+     * @param string $text_raw
+     * @param bool $verified
      */
-    public static function createDocument(array $words, Models\Topic $topic, string $text_name, string $text_raw, bool $verified){
-        $document = Models\Document::create(['name' => $text_name, 'text' => $text_raw]);
+    public static function createDocument(array $words, Topic $topic, string $text_name, string $text_raw, bool $verified){
+        $document = Document::create(['name' => $text_name, 'text' => $text_raw, 'hash' => md5($text_raw)]);
         $document->verified = $verified;
 
         $word_keys = array_keys($words);
 
         foreach ($word_keys as $word) {
-            $temp = Models\Word::firstOrCreate(['name' => $word]);
+            $temp = Word::firstOrCreate(['name' => $word]);
             $document->words()->attach($temp->id, ['tf' => $words[$word]]);
         }
-
-
-//        $document->words()->saveMany($word_models);
 
         $topic->documents()->save($document);
     }
 
-    /**
-     * @return null
-     */
-    public function getTopics() {
-        return $this->topics;
+    public function analyze(bool $save = false){
+        if($topic_id = $this->documentExists()) return [Topic::find($topic_id)];
+
+        $words = (new TextNormalizer($this->text, $this->language))->getWords();
+
+        $matrix = new DocumentWordMatrixCreator($words, $this->language);
+
+        $V = (new SingularValueDecomposition($matrix->getMatrix()))->getV();
+
+        $results = (new TextCategorization($V))->getResults();
+
+        $topics = Topic::find($matrix->getTopicsByDocumentIndex($results));
+
+        if($save) $this->createDocument($words, $topics[0], $this->text_name, $this->text, false);
+
+        return $topics;
     }
+
+    public function save($topic_id){
+        if($this->documentExists()) throw new JsonException(trans('admin.text_exists'));
+        $words = (new TextNormalizer($this->text, $this->language))->getWords();
+        $this->createDocument($words, Topic::find($topic_id), $this->text_name, $this->text, true);
+    }
+
+    private function documentExists(){
+        $existing_document = Document::where('hash', md5($this->text))->first();
+        return $existing_document ? $existing_document->topic_id : 0;
+    }
+
 }
